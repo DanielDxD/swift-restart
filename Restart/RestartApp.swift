44282bf2c594fce21baa2d05dd2f9104c2ada9f9
Daniel Lopes <daniellopes@Daniels-iMac-Pro.local>
2022-05-16 23:16:59 -0300

    Initial setup

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Daniel Lopes on 16/05/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
