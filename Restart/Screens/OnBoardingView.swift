a95e5687e348474dd8fc9c6b712b09612251ac64
Daniel Lopes <daniel.dxdb@gmail.com>
2022-05-18 19:53:47 -0300

    Implement button drag gesture

--- Restart/Screens/OnBoardingView.swift
+++ Restart/Screens/OnBoardingView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct OnBoardingView: View {
     // MARK: - PROPERTY
     @AppStorage("onboarding") var isOnBoardingViewActive: Bool = true
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
         
     // MARK: - BODY
     var body: some View {
@@ -72,7 +74,7 @@ struct OnBoardingView: View {
                     HStack {
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         Spacer()
                     }
                     
@@ -90,14 +92,28 @@ struct OnBoardingView: View {
                         }
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnBoardingViewActive = false
-                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                }
+                                .onEnded { _ in
+                                    if buttonOffset > buttonWidth / 2 {
+                                        buttonOffset = buttonWidth - 80
+                                        isOnBoardingViewActive = false
+                                    } else {
+                                        buttonOffset = 0
+                                    }
+                                }
+                        )
                         
                         Spacer()
                     }
                 }
-                .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth,height: 80, alignment: .center)
                 .padding()
                 
             } // VSTACK
