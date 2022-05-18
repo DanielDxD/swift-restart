64ddf16021c474be1e1ee55739ab24db76fd89a8
Daniel Lopes <daniel.dxdb@gmail.com>
2022-05-17 22:34:07 -0300

    Finishing onBoarding layout

--- Restart/Screens/OnBoardingView.swift
+++ Restart/Screens/OnBoardingView.swift
@@ -34,9 +34,78 @@ struct OnBoardingView: View {
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 10)
+                    
                 }
                 // MARK: - CENTER
+                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                Spacer()
                 // MARK: - FOOTER
+                
+                ZStack {
+                    // PARTS OF CUSTOM BUTTON
+                    
+                    // 1. BACKGROUND (STATIC)
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // 2. CTA (STATIC)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    // 3. CAPSULE (DYNAMIC WIDTH)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        Spacer()
+                    }
+                    
+                    // 4. CIRCLE (DRAGGABLE)
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnBoardingViewActive = false
+                        }
+                        
+                        Spacer()
+                    }
+                }
+                .frame(height: 80, alignment: .center)
+                .padding()
+                
             } // VSTACK
         } //: ZSTACK
     }
