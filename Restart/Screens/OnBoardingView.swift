f1724026d7e219cad1bee5c8d90c0c64d0655b1a
Daniel Lopes <daniel.dxdb@gmail.com>
2022-05-19 19:26:41 -0300

    Add haptic feedback

--- Restart/Screens/OnBoardingView.swift
+++ Restart/Screens/OnBoardingView.swift
@@ -16,6 +16,8 @@ struct OnBoardingView: View {
     @State private var imageOffset: CGSize = .zero
     @State private var indicatorOpacity: Double = 1.0
     @State private var textTitle: String = "Share."
+    
+    let hapticFeedback = UINotificationFeedbackGenerator()
         
     // MARK: - BODY
     var body: some View {
@@ -151,10 +153,12 @@ struct OnBoardingView: View {
                                 .onEnded { _ in
                                     withAnimation(Animation.easeOut(duration: 0.4)) {
                                         if buttonOffset > buttonWidth / 2 {
+                                            hapticFeedback.notificationOccurred(.success)
                                             playSound(sound: "chimeup", type: "mp3")
                                             buttonOffset = buttonWidth - 80
                                             isOnBoardingViewActive = false
                                         } else {
+                                            hapticFeedback.notificationOccurred(.warning)
                                             buttonOffset = 0
                                         }
                                     }
