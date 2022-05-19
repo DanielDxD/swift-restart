9ee75eb9439c5a96104513333bde42df2116fedc
Daniel Lopes <daniel.dxdb@gmail.com>
2022-05-19 19:24:24 -0300

    Playing audio

---
+++ Restart/Utilities/AudioPlayer.swift
@@ -0,0 +1,22 @@
+//
+//  AudioPlayer.swift
+//  Restart
+//
+//  Created by Daniel Lopes on 19/05/22.
+//
+
+import Foundation
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("Could not play the song file.")
+        }
+    }
+}
